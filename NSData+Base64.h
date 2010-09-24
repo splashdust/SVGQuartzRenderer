282cba3ab222dc7764f286269c2ffa1c00ddb32d
Joacim Magnusson <joacim@splashdust.net>
2010-09-24 16:52:11 +0200

    Added basic support for pattern fills

---
+++ NSData+Base64.h
@@ -0,0 +1,8 @@
+// Copy pasted from http://www.cocoadev.com/index.pl?BaseSixtyFour
+
+@interface NSData (MBBase64)
+
++ (id)dataWithBase64EncodedString:(NSString *)string;     //  Padding '=' characters are optional. Whitespace is ignored.
+- (NSString *)base64Encoding;
+
+@end
